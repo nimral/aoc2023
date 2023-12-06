38315377fb68b334bb1168d87e682761b91f7f9f
Matěj Kocián <matej.kocian@gmail.com>
2023-12-06 11:53:23 +0100

    Add day 6

---
+++ d6_2.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::iter::zip;
+
+fn parse_line(line: &str) -> Vec<usize> {
+    return vec![line.split_once(":").unwrap().1.replace(" ", "").replace("\n", "").parse().unwrap()];
+}
+
+fn main() {
+    let binding = fs::read_to_string("i6.txt").unwrap();
+    let (time_str, dist_str) = binding.split_once("\n").unwrap();
+    let times = parse_line(time_str);
+    let dists = parse_line(dist_str);
+
+    let mut prod = 1;
+    for (time, dist) in zip(times, dists) {
+        let speed1 = ((time as f64) - ((time * time - 4 * dist) as f64).sqrt()) / 2.0;
+        let speed2 = ((time as f64) + ((time * time - 4 * dist) as f64).sqrt()) / 2.0;
+        let low = speed1.min(speed2);
+        let high = speed2.max(speed1);
+        let mut margin = (high.floor() as isize) - (low.ceil() as isize) + 1;
+        if low == low.ceil() {
+            margin -= 1;
+        }
+        if high == high.floor() {
+            margin -= 1;
+        }
+        prod *= margin;
+    }
+    println!("{}", prod);
+}
