9004fd5426dc78267f5113ba203c9e12399a456a
Matěj Kocián <matej.kocian@gmail.com>
2023-12-07 11:22:25 +0100

    Add day 7

---
+++ d7_2.rs
@@ -0,0 +1,71 @@
+use std::fs;
+use std::collections::HashMap;
+
+struct Hand {
+    card_values: Vec<usize>,
+    bid: usize,
+    poker_hand_type: usize,
+}
+
+
+fn build_hand(s: &str, card_order: &HashMap<char, usize>) -> Hand {
+    let x = s.split_once(" ").unwrap();
+    let card_str = x.0;
+    let mut max_poker_hand_type = 0;
+    for new_char in card_order.keys() {
+        let cvs = card_str.replace("J", &new_char.to_string()).chars().map(|x| card_order[&x]).collect::<Vec<usize>>();
+        let mut sorted_cvs = cvs.clone();
+        sorted_cvs.sort();
+        let mut uniq_sorted_cvs = sorted_cvs.clone();
+        uniq_sorted_cvs.dedup();
+        let num_uniq = uniq_sorted_cvs.len();
+        
+        // high card
+        let mut poker_hand_type = 0;
+        if sorted_cvs[0] == sorted_cvs[4] {
+            // five of a kind
+            poker_hand_type = 6;
+        } else if sorted_cvs[0] == sorted_cvs[3] || sorted_cvs[1] == sorted_cvs[4] {
+            // four of a kind
+            poker_hand_type = 5;
+        } else if num_uniq == 2 {
+            // full house
+            poker_hand_type = 4;
+        } else if num_uniq == 3 {
+            if sorted_cvs[0] == sorted_cvs[2] || sorted_cvs[1] == sorted_cvs[3] || sorted_cvs[2] == sorted_cvs[4] {
+                // three of a kind
+                poker_hand_type = 3;
+            } else {
+                // two pair
+                poker_hand_type = 2;
+            }
+        } else if num_uniq == 4 {
+            // one pair
+            poker_hand_type = 1;
+        }
+        max_poker_hand_type = max_poker_hand_type.max(poker_hand_type);
+    }
+
+    Hand {
+        card_values: card_str.chars().map(|x| card_order[&x]).collect::<Vec<usize>>(),
+        bid: x.1.parse().unwrap(),
+        poker_hand_type: max_poker_hand_type,
+    }
+}
+
+
+fn main() {
+    let card_order: HashMap<char, usize> = HashMap::from(
+        ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+            .iter().rev().enumerate().map(|(a, b)| (*b, a)).collect::<HashMap<char, usize>>()
+    );
+    let mut hands: Vec<Hand> = fs::read_to_string("i7.txt").unwrap().lines().map(|x| build_hand(x, &card_order)).collect();
+    hands.sort_by_key(|x| (x.poker_hand_type, x.card_values.clone()));
+
+    let mut sum = 0;
+    for (i, hand) in hands.iter().enumerate() {
+        sum += hand.bid * (i + 1);
+    }
+
+    println!("{}", sum);
+}
